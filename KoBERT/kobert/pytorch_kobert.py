8532abc9088422f15dcd5a9e9e6bbeab46bf16ec
dugunhee <rjsdl2210@naver.com>
2021-12-07 20:31:15 +0900

    Feat1&2 : sentiment analysis success

---
+++ KoBERT/kobert/pytorch_kobert.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+# Copyright 2019 SK T-Brain Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from zipfile import ZipFile
+
+import torch
+
+from transformers import BertModel
+import gluonnlp as nlp
+
+from KoBERT.kobert.utils import download as _download
+from KoBERT.kobert.utils import tokenizer
+
+pytorch_kobert = {
+    "url": "https://kobert.blob.core.windows.net/models/kobert/pytorch/kobert_v1.zip",
+    "fname": "kobert_v1.zip",
+    "chksum": "411b242919",  # 411b2429199bc04558576acdcac6d498
+}
+
+
+def get_pytorch_kobert_model(ctx="cpu", cachedir=".cache"):
+    # download model
+    model_info = pytorch_kobert
+    model_down = _download(
+        model_info["url"], model_info["fname"], model_info["chksum"], cachedir=cachedir
+    )
+
+    zipf = ZipFile(os.path.expanduser(model_down))
+    zipf.extractall(path=os.path.expanduser(cachedir))
+    model_path = os.path.join(os.path.expanduser(cachedir), "kobert_from_pretrained")
+    # download vocab
+    vocab_info = tokenizer
+    vocab_path = _download(
+        vocab_info["url"], vocab_info["fname"], vocab_info["chksum"], cachedir=cachedir
+    )
+    return get_kobert_model(model_path, vocab_path, ctx)
+
+
+def get_kobert_model(model_path, vocab_file, ctx="cpu"):
+    bertmodel = BertModel.from_pretrained(model_path, return_dict=False)
+    device = torch.device(ctx)
+    bertmodel.to(device)
+    bertmodel.eval()
+    vocab_b_obj = nlp.vocab.BERTVocab.from_sentencepiece(
+        vocab_file, padding_token="[PAD]"
+    )
+    return bertmodel, vocab_b_obj
+
+
+if __name__ == "__main__":
+    import torch
+    from pytorch_kobert import get_pytorch_kobert_model
+
+    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
+    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
+    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
+    model, vocab = get_pytorch_kobert_model()
+    sequence_output, pooled_output = model(input_ids, input_mask, token_type_ids)
+    print(pooled_output.shape)
+    print(vocab)
+    print(sequence_output[0])
---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, render_template
+from KoBERT.kobert.pytorch_kobert import get_kobert_model
+from KoBERT.kobert_hf.kobert_tokenizer import KoBERTTokenizer
+
+import numpy as np
+import torch
+import os
+import requests
+import json
+import torch.nn as nn
+from bert_sentiment_analysis import bertmodel, BERTDataset,tok,vocab, max_len,batch_size
+
+device = 'cpu'
+# model.to(torch.device('cuda'))
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=6,
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device), return_dict=False)
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+    def predict(self, input_sentence):
+
+        data = [input_sentence, '0']
+        dataset_predict = [data]
+        # print("1")
+        predict_test = BERTDataset(dataset_predict, 0, 1, tok, vocab, max_len, True, False)
+        test_dataloader = torch.utils.data.DataLoader(predict_test, batch_size=batch_size, num_workers=5)
+        # print("2")
+        model.eval()
+        # print("3")
+        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+            token_ids = token_ids.long().to(device)
+            segment_ids = segment_ids.long().to(device)
+            valid_length = valid_length
+            label = label.long().to(device)
+            out = model(token_ids, valid_length, segment_ids)
+            # print("model output: ")
+            # print(out)
+            test_eval = []
+            for i in out:
+                logits = i
+                logits = logits.detach().cpu().numpy()
+
+                if np.argmax(logits) == 0:
+                    test_eval.append("기쁨이")
+                elif np.argmax(logits) == 1:
+                    test_eval.append("불안이")
+                elif np.argmax(logits) == 2:
+                    test_eval.append("당황이")
+                elif np.argmax(logits) == 3:
+                    test_eval.append("슬픔이")
+                elif np.argmax(logits) == 4:
+                    test_eval.append("분노가")
+                elif np.argmax(logits) == 5:
+                    test_eval.append("상처가")
+            # return out
+            return test_eval[0]
+model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/')
+@app.route('/<string:input_sentence>')
+def input_test(input_sentence=None):  # put application's code here
+    return render_template('main.html', input_sentence=input_sentence)
+
+
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    # params = json.loads(request.get_data())
+    # params_str = ''
+    # for key in params.key:
+    #     params_str += 'key: {}, value: {}<br>'.format(key, params[key])
+    # return params_str
+
+    temp = request.form['input_sentence']
+    print("입력한 문장 : " + temp)
+    # data = [input_sentence, '0']
+    # dataset_predict = [data]
+
+    # model = BERTClassifier(bertmodel, 0.5).to(device)
+    # model.load_state_dict(torch.load('/Users/dugunhee/bert/drivemodel.pt'), strict=False)
+    model.load_state_dict(torch.load('/Users/dugunhee/PycharmProjects/BertFlask/model_state_dict.pt', map_location=torch.device('cpu')))
+    # model.eval()
+    out = model.predict(temp)
+    print("분석 결과 : " + out)
+    return ">> 입력하신 내용에서 " + out + " 느껴집니다."
+    # return ">> 입력하신 내용에서 " + temp + " 느껴집니다."
+
+if __name__ == '__main__':
+    app.run()
